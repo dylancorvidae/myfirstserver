7893037519e693fb10a9c3a870855c4746634acc
Dylan Corvidae <dylancorvidae@gmail.com>
2019-08-16 12:17:05 -0700

    heyworld

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/data', (request, response) => {
     const airplanes = {
         departure: Date.now(),
         canFly: true,
-        pilot: 'Well boop Trained'
+        pilot: 'Well booheyeyep Trained'
     };
     response.status(200).json(airplanes);
 });
